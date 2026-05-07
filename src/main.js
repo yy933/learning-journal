94091018e07c03c5f077cadf6ecb92f651696fc4
yy933 <116449560+yy933@users.noreply.github.com>
2026-05-07 15:54:55 +0800

    feat: render all tags in the filter section on home page

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ import { renderNavbar } from "./components/navbar.js";
 import { renderFooter } from "./components/footer.js";
 
 const postsGrid = document.querySelector(".posts-grid");
+const filterContainer = document.querySelector(".filter-section");
+const allTags = ["All", ...new Set(postsData.flatMap((post) => post.tags))];
+
+// render all tags in filter section
+filterContainer.innerHTML = allTags
+  .map(
+    (tag) =>
+      `
+  <button class="tag-btn ${tag === "All" ? "active" : ""}" 
+          type="button" 
+          aria-pressed="${tag === "All"}"
+          data-tag="${tag}">
+    ${tag}
+  </button>
+`,
+  )
+  .join("");
+
 
 renderNavbar();
 renderFooter();
