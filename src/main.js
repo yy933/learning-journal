777758182e0fe2a87ebde92f1282793170f76105
yy933 <116449560+yy933@users.noreply.github.com>
2026-05-07 16:19:37 +0800

    feat: update filtered by tags feature. Posts will be rendered when selected tags in filteer section are included.

--- src/main.js
+++ src/main.js
@@ -7,11 +7,19 @@ const postsGrid = document.querySelector(".posts-grid");
 const filterContainer = document.querySelector(".filter-section");
 const allTags = ["All", ...new Set(postsData.flatMap((post) => post.tags))];
 
+filterContainer.addEventListener("click", (e) => {
+  const btn = e.target.closest(".tag-btn");
+  if (!btn) return;
+  const tag = btn.dataset.tag;
+  handleFilter(tag);
+});
+
 // render all tags in filter section
-filterContainer.innerHTML = allTags
-  .map(
-    (tag) =>
-      `
+function renderAllTags() {
+  filterContainer.innerHTML = allTags
+    .map(
+      (tag) =>
+        `
   <button class="tag-btn ${tag === "All" ? "active" : ""}" 
           type="button" 
           aria-pressed="${tag === "All"}"
@@ -19,10 +27,34 @@ filterContainer.innerHTML = allTags
     ${tag}
   </button>
 `,
-  )
-  .join("");
+    )
+    .join("");
+}
 
+// handle filter
+function handleFilter(selectedTag) {
+  const filteredData =
+    selectedTag === "All"
+      ? postData
+      : postsData.filter((post) => post.tags.includes(selectedTag));
+
+  // re-render posts
+  renderPosts(filteredData, postsGrid);
+
+  // update UI
+  updateFilterBtns(selectedTag);
+}
+
+function updateFilterBtns(selectedTag) {
+  const btns = filterContainer.querySelectorAll(".tag-btn");
+  btns.forEach((btn) => {
+    const isActive = btn.dataset.tag === selectedTag;
+    btn.classList.toggle("active", isActive);
+    btn.setAttribute("aria-pressed", isActive);
+  });
+}
 
 renderNavbar();
 renderFooter();
 renderPosts(postsData, postsGrid);
+renderAllTags();
