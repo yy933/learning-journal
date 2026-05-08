41c00e32158fa75e103b3a1a0d505b6e6b20011d
yy933 <116449560+yy933@users.noreply.github.com>
2026-05-08 11:18:45 +0800

    fix: fix profile imag rendering in About page by passing correct source paths

--- src/about.js
+++ src/about.js
@@ -5,10 +5,14 @@ import { renderFooter } from "./components/footer";
 import { renderPosts, getOptimizedImage } from "./components/postRenderer";
 
 const profileImage = document.querySelector(".profile-section img");
-profileImage.src = getOptimizedImage(profileImage.src);
+
+function getOptimizedProfileImage() {
+  return profileImage.src = getOptimizedImage(profileImage.src, 400);
+}
 
 const postsGrid = document.querySelector(".posts-grid");
 
 renderNavbar();
 renderFooter();
 renderPosts(postsData, postsGrid, 5);
+getOptimizedProfileImage()
